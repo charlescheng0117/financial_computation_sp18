c17b1c21fbfafde743a7eed09cd15c3855fd0982
Charles Cheng <charlescheng0117@gmail.com>
2018-07-03 17:02:52 +0800

    update input file

--- hw5/run.sh
+++ hw5/run.sh
@@ -1,11 +1,13 @@
+FILE=2.in
+
 echo "Compiling source code"
 make
 echo "Done"
 echo "Demo Binomial Tree methods"
-time ./hw5_binomial < 1.in
+time ./hw5_binomial < $FILE
 echo "Demo Monte Carlo methods"
-time ./hw5_monte < 1.in
+time ./hw5_monte < $FILE
 echo "Demo for bonus 1"
-time ./hw5_bn1 < 1.in
+time ./hw5_bn1 < $FILE
 echo "Finished."
 make clean
